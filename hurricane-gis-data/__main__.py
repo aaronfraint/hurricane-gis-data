97b7cdf86aff25fe01364a37479d9f1c92761fb9
Aaron Fraint <afraint@cartodb.com>
2023-12-06 15:04:48 -0500

    rename, update

--- hurricane-gis-data/__main__.py
+++ hurricane-gis-data/__main__.py
@@ -1,4 +1,6 @@
-from scrape_forecasts import download_zips, extract_all_zip_files, cleanup
+import sys
+
+from scrape_forecasts import download_zips, extract_all_zip_files
 from import_downloaded_data import import_shapefiles_to_bq
 from find_hurricanes_to_scrape import url_for_every_storm
 
@@ -8,15 +10,24 @@ from find_hurricanes_to_scrape import url_for_every_storm
 #     "2023_idalia": "https://www.nhc.noaa.gov/gis/archive_forecast_results.php?id=al10&year=2023&name=Hurricane%20IDALIA"
 # }
 
+
+def main(process):
+    print(f"This is the main module, {process}")
+
+    if process == "download":
+        for url in url_for_every_storm():
+            print("-" * 40)
+            print(url)
+            print("-" * 40)
+
+            download_zips(url=url)
+
+    elif process == "extract":
+        extract_all_zip_files()
+
+    elif process == "bq":
+        import_shapefiles_to_bq()
+
+
 if __name__ == "__main__":
-    for url in url_for_every_storm():
-        print("-" * 40)
-        print(url)
-        print("-" * 40)
-
-        download_zips(url=url)
-
-
-    # extract_all_zip_files()
-    # import_shapefiles_to_bq()
-    # cleanup()
+    main(sys.argv[1])
