63cb37c8e536953597a16d6a369f08b2f3e67b26
Aaron Fraint <afraint@cartodb.com>
2023-12-06 15:05:15 -0500

    black

--- hurricane-gis-data/find_hurricanes_to_scrape.py
+++ hurricane-gis-data/find_hurricanes_to_scrape.py
@@ -2,21 +2,22 @@ import re
 from bs4 import BeautifulSoup as bs
 import requests
 
-DOMAIN = "https://www.nhc.noaa.gov" 
+DOMAIN = "https://www.nhc.noaa.gov"
 
 
 def _get_soup(url):
-    return bs(requests.get(url).text, 'html.parser')
+    return bs(requests.get(url).text, "html.parser")
 
 
 def _get_urls_for_single_year(year):
-
     urls = []
 
     soup = _get_soup(f"https://www.nhc.noaa.gov/gis/archive_forecast.php?year={year}")
 
-    for link in soup.findAll("a", attrs={'href': re.compile("archive_forecast_results")}):
-        url = DOMAIN + link.get('href').replace(" ", "%20")
+    for link in soup.findAll(
+        "a", attrs={"href": re.compile("archive_forecast_results")}
+    ):
+        url = DOMAIN + link.get("href").replace(" ", "%20")
         # print(url)
         urls.append(url)
 
@@ -24,10 +25,8 @@ def _get_urls_for_single_year(year):
 
 
 def url_for_every_storm():
-
     urls = []
     for year in range(2014, 2024):
-
         print("*" * 40)
         print(year)
         print("*" * 40)
