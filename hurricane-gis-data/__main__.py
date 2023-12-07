36711247f6a01e580213647d52f26fc506d07503
Aaron Fraint <afraint@cartodb.com>
2023-12-07 10:12:00 -0500

    rename files

--- hurricane-gis-data/__main__.py
+++ hurricane-gis-data/__main__.py
@@ -1,8 +1,8 @@
 import sys
 
-from scrape_forecasts import download_zips, extract_all_zip_files
-from import_downloaded_data import import_shapefiles_to_bq
-from find_hurricanes_to_scrape import url_for_every_storm
+from _scrape_forecasts import download_zips, extract_all_zip_files
+from _import_downloaded_data import import_shapefiles_to_bq
+from _find_hurricanes_to_scrape import url_for_every_storm
 
 # hurricanes = {
 #     # "2012_sandy": "https://www.nhc.noaa.gov/gis/archive_forecast_results.php?id=al18&year=2012&name=Hurricane%20SANDY",
